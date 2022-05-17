7763e039f57267b13e30850434828bbcc32125b9
EstherL6 <91962646+EstherL6@users.noreply.github.com>
2022-05-17 16:07:19 +0200

    json
    imported json

--- main.go
+++ main.go
@@ -16,6 +16,15 @@ func homeLink(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	jsonFile, err := os.Open("spot.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Opened spot.json")
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
 	router.HandleFunc("/event", createEvent).Methods("POST")
@@ -27,10 +36,52 @@ func main() {
 }
 
 // create the strcture of the database in JSON
-type event struct {
-	ID          string `json:"ID"`
-	Title       string `json:"Title"`
-	Description string `json:"Description"`
+
+type Event struct {
+	Records []Record `json:"records,omitempty"`
+}
+
+type Record struct {
+	ID          *string `json:"id,omitempty"`         
+	CreatedTime *string `json:"createdTime,omitempty"`
+	Fields      *Fields `json:"fields,omitempty"`     
+}
+
+type Fields struct {
+	SurfBreak               []string `json:"Surf Break,omitempty"`               
+	DifficultyLevel         *int64   `json:"Difficulty Level,omitempty"`         
+	Destination             *string  `json:"Destination,omitempty"`              
+	Geocode                 *string  `json:"Geocode,omitempty"`                  
+	Influencers             []string `json:"Influencers,omitempty"`              
+	MagicSeaweedLink        *string  `json:"Magic Seaweed Link,omitempty"`       
+	Photos                  []Photo  `json:"Photos,omitempty"`                   
+	PeakSurfSeasonBegins    *string  `json:"Peak Surf Season Begins,omitempty"`  
+	DestinationStateCountry *string  `json:"Destination State/Country,omitempty"`
+	PeakSurfSeasonEnds      *string  `json:"Peak Surf Season Ends,omitempty"`    
+	Address                 *string  `json:"Address,omitempty"`                  
+}
+
+type Photo struct {
+	ID         *string     `json:"id,omitempty"`        
+	Width      *int64      `json:"width,omitempty"`     
+	Height     *int64      `json:"height,omitempty"`    
+	URL        *string     `json:"url,omitempty"`       
+	Filename   *string     `json:"filename,omitempty"`  
+	Size       *int64      `json:"size,omitempty"`      
+	Type       *string     `json:"type,omitempty"`      
+	Thumbnails *Thumbnails `json:"thumbnails,omitempty"`
+}
+
+type Thumbnails struct {
+	Small *Full `json:"small,omitempty"`
+	Large *Full `json:"large,omitempty"`
+	Full  *Full `json:"full,omitempty"` 
+}
+
+type Full struct {
+	URL    *string `json:"url,omitempty"`   
+	Width  *int64  `json:"width,omitempty"` 
+	Height *int64  `json:"height,omitempty"`
 }
 
 type allEvents []event
@@ -38,11 +89,23 @@ type allEvents []event
 // database according to the structure
 var events = allEvents{
 	{
-		ID:          "1",
-		Title:       "Introduction to Golang",
-		Description: "Come join us for a chance to learn how golang works and get to eventually try it out",
+		id: "1",
+		SurfBreak: ["Reef Break"],
+		DifficultyLevel: "4",
+		Destination: "Hawaii",
+		Coordinates: {
+			Latitude: "0",
+			Longitude: "1",
+		},
+		MagicSeaweed: "https://fr.magicseaweed.com/",
+		Photos: "https://images.unsplash.com/photo-1502680390469-be75c86b636f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+		SeasonStart: "27 juin 2022",
+		SeasonEnd: "23 octobre 2022",
+		Address: "île de Hawaii",
 	},
 }
+
+
 
 // post request createEvent
 func createEvent(w http.ResponseWriter, r *http.Request) {
